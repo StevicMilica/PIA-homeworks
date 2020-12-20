cf714d83c78e5c8ffbfd44cc407be1fd0aa7e722
StevicMilica <74367047+StevicMilica@users.noreply.github.com>
2020-12-20 02:49:18 +0100

    Cuvanje rezultata
    Uvedena funkcionalnost za cuvanje rezultata.
    Rezultati se cuvaju automatski po odustajanju/zavrsetku kviza.

--- hw-3/main.js
+++ hw-3/main.js
@@ -2,6 +2,10 @@ let questionsData = null;
 let correctCount = 0;
 let userAnswers = []
 let qIndex = 0;
+$(document).ready(function () {
+
+    $("body").fadeIn();
+})
 let loadQuestionData = function (index) {
     $("#qNumber").text("Pitanje br. " + questionsData[index].question_id)
     $("#qText").text("Pitanje: " + questionsData[index].question_text)
@@ -25,46 +29,85 @@ $.ajax({
 
 
 
-let getNextQuestion = function () {
-    let inputs = $("input");
-    for (let i = 0; i < inputs.length; i++) {
-        $(inputs[i]).prop("checked", false);
-    }
-    if (qIndex < questionsData.length - 1) {
-        $("#qNext").prop("disabled", false);
-        $("#qNext").removeClass("btn-secondary");
-        $("#qNext").addClass("btn-info");
-        $("#qPrev").removeClass("btn-secondary");
-        $("#qPrev").addClass("btn-info");
-        qIndex++;
-        loadQuestionData(qIndex);
-        checkIfAnswered(qIndex);
 
-    } else {
-        $("#qNext").prop("disabled", true);
-        $("#qNext").addClass("btn-secondary");
-        $("#qNext").removeClass("btn-info");
-        alert(`Kviz je gotov. Imali ste ${correctCount} od ${questionsData.length} poena`)
-    }
-    console.log(userAnswers)
-}
-let getPrevQuestion = function () {
-    if (qIndex > 0) {
-        $("#qPrev").prop("disabled", false);
-        $("#qPrev").removeClass("btn-secondary");
-        $("#qPrev").addClass("btn-info");
-        $("#qNext").removeClass("btn-secondary");
-        $("#qNext").addClass("btn-info");
-        qIndex--;
-        loadQuestionData(qIndex)
-        checkIfAnswered(qIndex);
 
+$("#qNext").click(function () {
+    let answerOptions = $("input")
+    let result = validateAnswer(answerOptions)
+    if (result) {
+        $("#answerCorrect").slideToggle();
+        $("#answerIncorrect").hide();
+        correctCount++;
     } else {
-        $("#qPrev").prop("disabled", true);
-        $("#qPrev").addClass("btn-secondary");
-        $("#qPrev").removeClass("btn-info");
+        $("#answerCorrect").hide();
+        $("#answerIncorrect").slideToggle();
+    }
+    setTimeout(function () {
+        $("#answerCorrect").slideUp();
+        $("#answerIncorrect").slideUp();
+        getNextQuestion();
+    }, 1000);
+})
+
+$("#qSkip").click(function () {
+    let answerOptions = $("input")
+    userAnswers[qIndex] = -1;
+    getNextQuestion();
+})
+$("#giveUp").click(function () {
+    finishQuiz();
+    qIndex = 0;
+
+})
+$("#quizStart").click(function () {
+    if ($("#userName").val() == '') {
+        alert("Korisnicko ime je obavezno, unesite ga u polju ispod")
+    } else {
+        let username = $("#userName").val();
+        sessionStorage.setItem('username', username)
+        $("#userData").slideToggle();
+        $("#quizContainer").slideDown();
+
+    }
+})
+$("#reset").click(function () {
+    reset();
+})
+let showResults = function () {
+    writeResults();
+    $("#resultData").slideToggle();
+}
+let writeResults = function () {
+    let results = getResults();
+    for (let i = 0; i < results.length; i++) {
+        let element = "<tr><td>" + results[i].userName + "</td><td>" + results[i].result + "</td></tr>"
+        $("#resultsBody").append(element);
     }
 }
+let getResults = function () {
+    let res = JSON.parse(localStorage.getItem("results"));
+    if (res == null) {
+        res = []
+    }
+    return res;
+}
+
+let storeResults = function () {
+    let results = getResults();
+    let userName = sessionStorage.getItem("username");
+    let result = correctCount;
+    let userProfile = {
+        "userName": userName,
+        "result": result
+    }
+    results.push(userProfile)
+    localStorage.setItem("results", JSON.stringify(results));
+}
+
+let reset = function () {
+    location.reload();
+}
+
 let validateAnswer = function (answOpt) {
     let answer = null;
     let answered = false;
@@ -72,7 +115,6 @@ let validateAnswer = function (answOpt) {
         if ($(answOpt[i]).is(":checked")) {
             let indChecked = i;
             answer = $("label[for = '" + $(answOpt[i]).attr("id") + "']").text();
-            console.log(answer)
             userAnswers[qIndex] = indChecked;
             answered = true;
             break;
@@ -88,82 +130,33 @@ let validateAnswer = function (answOpt) {
     }
 
 }
-let checkIfAnswered = function (qInd) {
-    let qOptions = $("input");
-    if (userAnswers[qInd] != undefined) {
-        console.log(`qInd = ${qInd}`)
-        console.log(qOptions[qInd])
-        $(qOptions[userAnswers[qInd]]).prop('checked', true);
+let getNextQuestion = function () {
+    let finish = false;
+    let inputs = $("input");
+    for (let i = 0; i < inputs.length; i++) {
+        $(inputs[i]).prop("checked", false);
+    }
+    if (qIndex < questionsData.length) {
+        if (qIndex < questionsData.length - 1) {
+            qIndex++;
+            loadQuestionData(qIndex);
+        } else {
+            qIndex++;
+        }
+
+    }
+    if (qIndex == questionsData.length - 1) {
+        $("#qNext").addClass("btn-success");
+        $("#qNext").removeClass("btn-info");
+        $("#qNext").text("Zavrsi kviz");
+
+    }
+    if (qIndex == questionsData.length) {
+        finishQuiz();
     }
 }
-$("#qNext").click(function () {
-    let answerOptions = $("input")
-    console.log(answerOptions)
-    let result = validateAnswer(answerOptions)
-    if (result) {
-        $("#answerCorrect").slideToggle();
-        $("#answerIncorrect").hide();
-
-        correctCount++;
-    } else {
-        $("#answerCorrect").hide();
-        $("#answerIncorrect").slideToggle();
-    }
-    setTimeout(function () {
-        $("#answerCorrect").slideUp();
-        $("#answerIncorrect").slideUp();
-
-        getNextQuestion();
-    }, 1000);
-})
-$("#qPrev").click(function () {
-    getPrevQuestion();
-})
-$("#qSkip").click(function () {
-    let answerOptions = $("input")
-    console.log(answerOptions)
-    userAnswers[qIndex] = -1;
-    getNextQuestion();
-})
-$("#giveUp").click(function(){
+let finishQuiz = function () {
     storeResults();
     showResults();
-    qIndex = 0;
-    $("#quizContainer").hide();
-    $("#resultData").show();
-})
-$("#quizStart").click(function () {
-    if ($("#userName").val() == '') {
-        alert("Korisnicko ime je obavezno, unesite ga u polju ispod")
-    }
-    else{
-        // console.log("Promenjen")
-        let username = $("#userName").val();
-        sessionStorage.setItem('username',username)
-        $("#userData").slideToggle();
-        $("#quizContainer").slideDown();
-        
-    }
-})
-let showResults = function(){
-    $("#results").show();
-    console.log(localStorage)
-}
-
-let storeResults = function () {
-    let results = JSON.parse(localStorage.getItem("results"));
-    if(results == null){
-        results = []
-    }
-    console.log(results)
-    let userName = sessionStorage.getItem("username");
-    let result = correctCount;
-    let userProfile = {
-        "userName": userName,
-        "result": result
-    }
-    console.log(userProfile)
-    results.push(userProfile)
-    console.log(results)
-    localStorage.setItem("results", JSON.stringify(results));
+    $("#quizContainer").slideToggle();
 }
