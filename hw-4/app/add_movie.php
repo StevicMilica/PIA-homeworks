2b50bd008b83050c9a4f7822ccac2bb3a593ac09
StevicMilica <74367047+StevicMilica@users.noreply.github.com>
2021-01-30 05:18:57 +0100

    Izmena layouta za dodavanje filma
    Izmenjen layout za dodavanje filma radi bolje efikasnosti i preglednosti pri dodavanju.

--- hw-4/app/add_movie.php
+++ hw-4/app/add_movie.php
@@ -11,18 +11,18 @@
     <?php include('server.php')?>
     <?php include('nav.php')?>
     <?php include('grantAccess.php')?>
-    <h1 class="text-center mt-3">Dodaj Film</h1>
-    <div class="content container">
-<?php if(isset($_SESSION['errors'])):?>
-            <div class="errors text-center alert-danger">
-                <?php foreach($_SESSION['errors'] as $error): ?>
-                <h6><?php echo $error; ?></h6><br>
-                <?php endforeach ?>
-                <?php unset($_SESSION['errors']) ?>
-            </div>
-            <?php endif ?>
+    <h1 class="text-center mt-3">Dodaj Film</h1><br>
+    <div class="content container w-50">
+        <?php if(isset($_SESSION['errors'])):?>
+        <div class="errors text-center alert-danger">
+            <?php foreach($_SESSION['errors'] as $error): ?>
+            <h6><?php echo $error; ?></h6><br>
+            <?php endforeach ?>
+            <?php unset($_SESSION['errors']) ?>
+        </div>
+        <?php endif ?>
         <form method="post" action='server.php' enctype="multipart/form-data">
-            
+
             <div class="input-group mb-3">
                 <input type="text" name="title" class="form-control" placeholder="Naziv filma">
             </div>
@@ -32,24 +32,28 @@
             <div class="input-group mb-3">
                 <input type="text" name="genre" class="form-control" placeholder="Zanr">
             </div>
-            <div class="input-group mb-3">
-                <input type="text" name="screenwriter" class="form-control" placeholder="Scenarista">
-            </div>
-            <div class="input-group mb-3">
-                <input type="text" name="director" class="form-control" placeholder="Reziser">
-            </div>
-            <div class="input-group mb-3">
-                <input type="text" name="production_house" class="form-control" placeholder="Producentska kuca">
+            <div class="row">
+                <div class="col-md-4 mb-3">
+                    <input type="text" name="screenwriter" class="form-control" placeholder="Scenarista">
+                </div>
+                <div class="col-md-4 mb-3">
+                    <input type="text" name="director" class="form-control" placeholder="Reziser">
+                </div>
+                <div class="col-md-4 mb-3">
+                    <input type="text" name="production_house" class="form-control" placeholder="Producentska kuca">
+                </div>
             </div>
             <div class="input-group mb-3">
                 <input type="text" name="actors" class="form-control" placeholder="Lista glumaca">
             </div>
 
-            <div class="input-group mb-3">
-                <input type="number" name="duration" class="form-control" placeholder="Trajanje u minutima">
-            </div>
-            <div class="input-group mb-3">
-                <input type="number" name="release_year" class="form-control" placeholder="Godina izdanja">
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <input type="number" name="duration" class="form-control" placeholder="Trajanje u minutima">
+                </div>
+                <div class="col-md-6 mb-3">
+                    <input type="number" name="release_year" class="form-control" placeholder="Godina izdanja">
+                </div>
             </div>
             <div class="input-group mb-3">
                 <input type="file" name="poster" class="form-control" placeholder="Slika">
@@ -61,7 +65,8 @@
         </form>
     </div>
 
-<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"
+        integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
